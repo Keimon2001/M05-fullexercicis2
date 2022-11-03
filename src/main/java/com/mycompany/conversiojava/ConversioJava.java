c364fa83408c405bdeb7fb051959cde349767350
DAM <DAM@203-24>
2022-11-03 15:45:37 +0000

    Hi havien alguns errors d'ortografia, ja corregits.

--- src/main/java/com/mycompany/conversiojava/ConversioJava.java
+++ src/main/java/com/mycompany/conversiojava/ConversioJava.java
@@ -14,7 +14,7 @@ public class ConversioJava {
         int valor_euros = sc.nextInt();
                 
         double tipus_de_canvi = 0.87d;
-        System.out.println(valor_euros + " eur0s (€) acatualmentt ewuivalen a" + (valor_euros * tipus_de_canvi) + " lliures esterlines (£)");
+        System.out.println(valor_euros + " euros (€) acatualment equivalen a" + (valor_euros * tipus_de_canvi) + " lliures esterlines (£)");
     }
 }
 
