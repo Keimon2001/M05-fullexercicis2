a4b94408e0c6e897ce65f54e47441892e4ef133c
DAM <DAM@203-24>
2022-11-03 15:44:48 +0000

    Hi havia un error en la conversió de Euro a Lliura. Ja esta solucionat.

---
+++ src/main/java/com/mycompany/conversiojava/ConversioJava.java
@@ -0,0 +1,20 @@
+
+
+package com.mycompany.conversiojava;
+
+import java.util.Scanner;
+
+
+public class ConversioJava {
+
+         public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Introdueix en €");
+        int valor_euros = sc.nextInt();
+                
+        double tipus_de_canvi = 0.87d;
+        System.out.println(valor_euros + " eur0s (€) acatualmentt ewuivalen a" + (valor_euros * tipus_de_canvi) + " lliures esterlines (£)");
+    }
+}
+
